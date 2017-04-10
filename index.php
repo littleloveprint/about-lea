d0cb1e0fd83a291400bcac76638f52037fc9b52e
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-04-10 11:01:38 -0600

    First run

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>About Lea</title>
+	</head>
+	<body>
+		<p><img src="/images/20161203_160258-2.jpg">
+			<br>
+			Words go here.</p>
+		<br>
+		<p>More words here.</p>
+		<br>
+		<p>Even more words.</p>
+	</body>
+</html>
