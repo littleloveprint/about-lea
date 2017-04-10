7706e5675c013a42176d07e88ecac85ff66fd011
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-04-10 13:01:43 -0600

    First run

--- index.php
+++ index.php
@@ -7,9 +7,11 @@
 		<h1>All About Lea</h1>
 		<img src="images/aboutlea.jpg" alt="Lea" height="192" width="144">
 			<br>
-		<p>Words go here. Lots and lots of words.
-			<br>Word, word, word, word, word, word.
-			<br>Blah blah blah. Blah blah blah.</p>
+		<p>For the past ten years, I have worked
+			<br>in graphic design and brand development.
+			<br>Most work I've done has to do with
+			<br>both web design and creating
+			<br>marketing and promotional materials.</p>
 		<p>Even more words go right here.
 		<br>Now these words will <strong>ABSOLUTELY</strong> be profound!</p>
 		<p>Most Important Things to Know
