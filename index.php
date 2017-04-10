890efdecd3c96fced2a0cd21c7052aef50a6d0bd
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-04-10 11:44:12 -0600

    First run

--- index.php
+++ index.php
@@ -4,12 +4,19 @@
 		<title>About Lea</title>
 	</head>
 	<body>
-		<p><img src="/images/aboutlea.jpg">
+		<h1>All About Lea</h1>
+		<img src="images/aboutlea.jpg" alt="Lea" height="192" width="144">
 			<br>
-			Words go here.</p>
-		<br>
-		<p>More words here.</p>
-		<br>
-		<p>Even more words.</p>
+		<p>Words go here. Lots and lots of words.
+			<br>Word, word, word, word, word, word.
+			<br>Blah blah blah. Blah blah blah.</p>
+		<p>Even more words go right here.
+		<br>Now these words will <strong>ABSOLUTELY</strong> be profound!</p>
+		<p>Most Important Things to Know
+		<ul>
+			<li>One</li>
+			<li>Two</li>
+			<li>Three</li>
+		</ul>
 	</body>
 </html>
